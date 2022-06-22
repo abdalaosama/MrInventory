70b3b1f83163aff13ba8e135bb0a0feafa0a034e
abdalaosama <abdalaosamah@gmail.com>
2022-06-22 14:54:03 -0400

    added Settings page

--- components/settings.js
+++ components/settings.js
@@ -1,8 +1,21 @@
-import { View, Text } from "react-native"
+import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native"
 
 export default function SettingsScreen(props){
     
     return (
-        <View></View>
+        <View style={{flex:1, padding:5, backgroundColor:"white" }}>
+            <View style={{height:70, flexDirection:"row", justifyContent:"space-between", alignItems:"center"}}>
+                <TouchableOpacity>
+                    <Image source={require('../images/back.png')}  />
+                </TouchableOpacity>
+                <Text style={{ fontSize:20 }}> Settings </Text>
+                <View></View>
+            </View>
+            <ScrollView style={{ flex:1, padding:10}}>
+                <View style={{flex:1}}>
+                    
+                </View>
+            </ScrollView>
+        </View>
     )
 }
