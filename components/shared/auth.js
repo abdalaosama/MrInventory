c644f5466986601e87a33b32fd6a736d1bc03409
abdalaosama <abdalaosamah@gmail.com>
2022-07-25 12:28:01 -0400

    chore: added scafolding for some functions

---
+++ components/shared/auth.js
@@ -0,0 +1,30 @@
+let auth;
+
+auth.login = function (username, password){
+
+}
+
+auth.getPublicKey = function(){
+
+}
+
+auth.CheckLogin = function(){
+
+}
+
+auth.activate = function(){
+
+    
+}
+
+auth.doFullCheck = function (){
+
+}
+
+auth.doOnBootCheck = function(){
+
+}
+
+
+
+module.exports = auth;
