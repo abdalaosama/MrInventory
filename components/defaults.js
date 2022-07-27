2c89d16fc2feb445c452a07f688e285840fbd08d
abdalaosama <abdalaosamah@gmail.com>
2022-07-27 19:59:43 -0400

    chore: changed default configs for the app

--- components/defaults.js
+++ components/defaults.js
@@ -1,7 +1,7 @@
  
 
 const defaultAppSettings = {
-    AuthServerHost: "http://167.172.172.65/",
+    AuthServerHost: "http://167.172.172.65",
     itemCatalogFile: "/storage/emulated/0/admin/items.txt",
     ExportFilesPath: "/admin",
 
