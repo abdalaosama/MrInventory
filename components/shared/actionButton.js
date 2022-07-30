cd3d37120347364133183074ac0492e44d0cd4c4
abdalaosama <abdalaosamah@gmail.com>
2022-07-30 16:26:46 -0400

    fix: onPress event wasn't registered

--- components/shared/actionButton.js
+++ components/shared/actionButton.js
@@ -4,7 +4,7 @@ import { View, Text, TouchableOpacity} from 'react-native';
 export default function ActionButton(props){
 
     return (
-        <TouchableOpacity style={[{width:"100%", height:50, backgroundColor:props.buttonColor || "#52C1FF" , alignItems:"center", justifyContent:"center", borderRadius: 5}, props.style]} >
+        <TouchableOpacity style={[{width:"100%", height:50, backgroundColor:props.buttonColor || "#52C1FF" , alignItems:"center", justifyContent:"center", borderRadius: 5}, props.style]} onPress={props.onPress}>
             <Text style={{color:"white"}}>{props.label}</Text>
         </TouchableOpacity>
     )
