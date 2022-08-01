83a6585e5f1e10290dc6fa877e4cef2f0facc021
abdalaosama <abdalaosamah@gmail.com>
2022-08-01 16:43:36 -0400

    feat: implmented License check not functional yet

--- components/login.js
+++ components/login.js
@@ -16,6 +16,71 @@ export default function LoginScreen ( props ){
     const [LoadingMessage, setLoadingMessage] = react.useState("");
     const [progressAnimVal, SetProgressAnimVal] = react.useState(0);
 
+    async function DoLicenseCheckRouting(){
+        try {
+            
+        
+            // await auth.logout();
+            const LoggedIn = await auth.CheckLogin();
+            console.log(`User is Logged in ? : ${LoggedIn}`)
+            setLoggedIn(LoggedIn);
+            setLoadingMessage('Checking if you are logged in...');
+            if(!LoggedIn) return setLoadingMessage('You are not Logged in') ;
+            
+            // check time for choice of full check or just offline check
+            //get last_check from secure store
+            const last_check = await SecureStore.getItemAsync('last_check');
+            setLoadingMessage('Checking Last open...');
+            // if last_check is null, then set it to todays date
+            let check = 'offline';
+            if (last_check == null) {
+                await SecureStore.setItemAsync('last_check', new Date().toISOString());
+                check = 'full';
+            }
+            // if check is more than a day old, then set it to full
+            
+            const last_check_date = new Date(last_check);
+            const today = new Date();
+            const diff = today.getTime() - last_check_date.getTime();
+            const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+            if (days > 1) {
+                await SecureStore.setItemAsync('last_check', new Date().toISOString());
+                check = 'full';
+            }
+            
+            setLoadingMessage(check == "full"?'Checking License with server...':"Checking License...");
+            
+
+
+            let activated = false;
+            activated = await auth.doLicenseCheck(check);
+            // active ?
+            if(activated){
+                // go to store choice screen
+                return props.navigation.navigate('stores');
+            }
+
+            setLoadingMessage("Attempting to Activate...");
+            await auth.activate();
+
+            activated = await auth.doLicenseCheck();
+            if(activated){
+                // go to store choice screen
+                return props.navigation.navigate('stores');
+            }  
+
+            setLoadingMessage("Activation Failed");
+            // show app inactive screen
+        } catch (error) {
+            console.log(error);
+            setLoadingMessage("Error, please try again...");
+            setTimeout(() => {
+                // auth.logout();
+                setLoggedIn(false);
+            }, 1000)
+        }
+    }
+
 
     react.useEffect(()=> {
         setTimeout(() => {
@@ -26,68 +91,11 @@ export default function LoginScreen ( props ){
 
     react.useEffect(() => {
         (async () => {
-            try {
-                const LoggedIn = await auth.CheckLogin();
-                console.log(LoggedIn)
-                setLoggedIn(LoggedIn);
-                setLoadingMessage('Checking if you are logged in...');
-                if(!LoggedIn) return;
-                
-                // check time for choice of full check or just offline check
-                //get last_check from secure store
-                const last_check = await SecureStore.getItemAsync('last_check');
-                setLoadingMessage('Checking Last open...');
-                // if last_check is null, then set it to todays date
-                let check = 'offline';
-                if (last_check == null) {
-                    await SecureStore.setItemAsync('last_check', new Date().toISOString());
-                    check = 'full';
-                }
-                // if check is more than a day old, then set it to full
-                
-                const last_check_date = new Date(last_check);
-                const today = new Date();
-                const diff = today.getTime() - last_check_date.getTime();
-                const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-                if (days > 1) {
-                    await SecureStore.setItemAsync('last_check', new Date().toISOString());
-                    check = 'full';
-                }
-                
-                setLoadingMessage(check == "full"?'Checking License with server...':"Checking License...");
-            
-                let activated = false;
-                switch(check){
-                    case 'full':
-                        activated = await auth.doFullCheck();
-                        break;
-                    case 'offline':
-                        activated = await auth.doOnBootCheck();
-                        break;
-                }
-
-                // do check
-                // active ?
-                    // go to store choice screen
-                // try to activate 
-                // succeed ?
-                    // jump back to do check
-                // show app inactive screen
-            } catch (error) {
-                console.log(error);
-                setLoadingMessage("Error, please try again...");
-                setTimeout(() => {
-                    auth.logout();
-                    setLoggedIn(false);
-                }, 1000)
-            }
-            
-            // check if you are logged in
-            
+            // await auth.logout();
+            // console.log(DeviceInfo)
+            await DoLicenseCheckRouting();
         })();
         return () => {
-
-
         }
     }, [])
 
@@ -96,6 +104,7 @@ export default function LoginScreen ( props ){
         const LoggedIn = await auth.CheckLogin();
         console.log(LoggedIn)
         setLoggedIn(LoggedIn);
+        DoLicenseCheckRouting();
     }
     return (
     <ScrollView style={{flex:1}}>
--- components/utils/auth.js
+++ components/utils/auth.js
@@ -1,8 +1,9 @@
 import defaultAppSettings from '../defaults';
 import * as SecureStore from 'expo-secure-store';
-
-const { AuthServerHost } = defaultAppSettings;
-
+import crypt from './crypto';
+import { getUniqueId } from 'react-native-device-info';
+const { AuthServerHost, LicenseFilePath } = defaultAppSettings;
+import * as rnfs from 'react-native-fs';
 export async function login (username, password){
     try {
         const body = {"username":username,"password":password};
@@ -64,35 +65,67 @@ export async function CheckLogin (){
     else return false
 }
 
-export function activate (){
-
+export async function activate (){
+    try {
     
+        //get the license key
+        let getKeyResp = await fetch(`${AuthServerHost}/v1/get_key`, {method:"GET"});
+        getKeyResp = await getKeyResp.json();
+        // console.log(getKeyResp);
+        if(getKeyResp.status == 0){
+            //if license and kay exists save the values to secure store
+            await SecureStore.setItemAsync('license_key', getKeyResp.key.license_key);
+            if(getKeyResp.key.license){
+                rnfs.writeFile(LicenseFilePath, getKeyResp.key.license, 'utf8');
+            }
+        }
+
+        let activateResp = await fetch(`${AuthServerHost}/v1/authenticate_license`, {
+            "method": "POST",
+            "headers": {
+              "Content-Type": "application/json"
+            },
+            "body": JSON.stringify({ "license_key": await SecureStore.getItemAsync('license_key'),"finger_print": getUniqueId()})
+          })
+
+        activateResp = await activateResp.json();
+        console.log(activateResp);
+        
+        if(activateResp.status == 0){
+            rnfs.writeFile(LicenseFilePath, activateResp.license, 'utf8');
+            return true;
+        }
+
+        return false
+        //     
+    } catch (error) {
+        console.log(error)
+        return false;
+    }
 }
 
-export function doFullCheck  (){
+export async function doLicenseCheck (full = false){
+    
     try {
-        let result = false;   
-            // check if the license Parameters Exists
-            // decrypt 
-            // check expiration time
-            // check finger print
-            // check license server
-   
-       } catch (error) {
-           console.log(error)
-           return false;
-       }
-   
-}
+        let result = false;
+        
+        const PublicKey = await getPublicKey();
+        if ( !PublicKey ) return false;
+        
+        const license = await rnfs.readFile(LicenseFilePath, 'utf8');
+        if ( !license ) return false;
 
-export function doOnBootCheck (){
-    try {
-     let result = false;   
-        // check if the license Parameters Exists
+        // console.log(license);
+        const license_key = await SecureStore.getItemAsync('license_key');
+        if ( !license_key ) return false;
+
         // decrypt 
+        const DecryptedLicense = await crypt(PublicKey, license, false);
+
+        console.log(DecryptedLicense);
+
         // check expiration time
         // check finger print
-    
 
     } catch (error) {
         console.log(error)
