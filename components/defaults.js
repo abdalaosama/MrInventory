345da29a9fcfbf6116d03150e5deecbf8d38eea1
abdalaosama <abdalaosamah@gmail.com>
2022-08-01 16:42:56 -0400

    feat: license is now stored in a file

--- components/defaults.js
+++ components/defaults.js
@@ -4,6 +4,7 @@ const defaultAppSettings = {
     AuthServerHost: "http://167.172.172.65",
     itemCatalogFile: "/storage/emulated/0/admin/items.txt",
     ExportFilesPath: "/admin",
+    LicenseFilePath: "/storage/emulated/0/admin/License.txt",
 
     ScanCoolDown: 2000,
     store: "1",
